3dc71d7b3034c8aa24592f093908add69daef673
chiragsardana <chiragsardana12@gmail.com>
2022-04-15 20:34:51 +0530

    database added

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 // This is App.js 
 const express = require("express");
+const mongoose = require('./db/connect');
 const res = require("express/lib/response");
 const app = express(); // express function and it return an app fn
-
+app.use(express.static("public"));
 require("dotenv").config();
 
 
